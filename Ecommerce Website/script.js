dc4ec64afe4fc863247ff36b308dead3c3408594
umair-iqbal-61 <ui0886342@gmail.com>
2025-07-13 16:00:00 +0500

    product page completed

--- Ecommerce Website/script.js
+++ Ecommerce Website/script.js
@@ -1,3 +1,5 @@
+// Home Page
+
 const bar = document.querySelector('#bar')
 const close = document.querySelector('.close')
 const nav = document.querySelector('.nav-items')
@@ -6,10 +8,28 @@ if (bar) {
     bar.addEventListener('click', () => {
         nav.classList.add('active')
     })
-}
+};
 
 if (close) {
     close.addEventListener('click', () => {
         nav.classList.remove('active')
     })
+};
+
+// Product Page
+
+var mainImg = document.querySelector('.main-img')
+var smallImg = document.querySelectorAll('.small-img')
+
+smallImg[0].onclick = function () {
+    mainImg.src = smallImg[0].src
+}
+smallImg[1].onclick = function () {
+    mainImg.src = smallImg[1].src
+}
+smallImg[2].onclick = function () {
+    mainImg.src = smallImg[2].src
+}
+smallImg[3].onclick = function () {
+    mainImg.src = smallImg[3].src
 }
